b321eb7868a561f68fc9a999509d749850cf3f6c
Maurice Lam <me@mauricelam.com>
2012-11-18 19:13:05 -0600

    A mapping from commands to readable command names

--- CheatSheet.py
+++ CheatSheet.py
@@ -6,10 +6,70 @@ import json
 settings = sublime.load_settings("KeymapManager.sublime-settings")
 
 
-def prettifySnake(str):
-    return str.replace('_', ' ').title()
+def prettifySnake(string):
+    return string.replace('_', ' ').title()
 
-keychars = {
+
+def dictString(dictionary):
+    if isinstance(dictionary, dict):
+        string = []
+        for key, value in dictionary.iteritems():
+            string.append(key + ': ' + dictString(value))
+        return '{' + ', '.join(string) + '}'
+    elif isinstance(dictionary, list):
+        return '[' + ', '.join(map(lambda x: dictString(x), dictionary)) + ']'
+    else:
+        return str(dictionary)
+
+
+def formatUnknownName(name, args):
+    string = prettifySnake(name)
+    if args is not None:
+        string += ' - ' + dictString(args)
+    return string
+
+
+def getCommandDisplayName(name, args):
+    # special case for set_layout
+    if name == 'set_layout':
+        cols = len(args['cols']) - 1
+        rows = len(args['rows']) - 1
+        description = '{cols} x {rows}'
+        if rows == 1 and cols == 1:
+            description = 'Single'
+        elif rows == 1:
+            description = '{cols} columns'
+        elif cols == 1:
+            description = '{rows} rows'
+        return 'Set Layout - ' + description.format(rows=rows, cols=cols)
+    if name in COMMANDS:
+        if isinstance(COMMANDS[name], str):
+            return COMMANDS[name]
+        else:
+            displayName = getValueInDict(COMMANDS[name], args)
+            return displayName or formatUnknownName(name, args)
+    else:
+        return formatUnknownName(name, args)
+
+
+def getValueInDict(dictionary, args):
+    if args is None:
+        return None
+    for key, value in args.iteritems():
+        if key not in dictionary:
+            continue
+        if isinstance(dictionary[key], str):
+            filenameext = os.path.basename(value)
+            filename = os.path.splitext(filenameext)[0]
+            return dictionary[key].format(value=value, pvalue=prettifySnake(value), filename=filename, filenameext=filenameext)
+        elif value in dictionary[key]:
+            if isinstance(dictionary[key][value], str):
+                return dictionary[key][value]
+            else:
+                return getValueInDict(dictionary[key][value], args)
+
+
+KEYCHARS = {
     'super': u'\u2318',
     'alt': u'\u2325',
     'shift': u'\u21e7',
@@ -27,7 +87,8 @@ keychars = {
     'forward_slash': '\\',
     'plus': '+',
     'equals': '=',
-    'minus': '-'
+    'minus': '-',
+    'backquote': '`'
 }
 
 
@@ -37,29 +98,69 @@ def prettifyKeys(keylist):
         keys = keystr.split('+')
         keystring = ''
         for key in keys:
-            keystring += keychars.get(key) or key.upper()
+            keystring += KEYCHARS.get(key) or key.upper()
         pretty.append(keystring)
     return '  '.join(pretty)
 
 
+COMMANDS = {
+    'show_overlay': {
+        'overlay': {
+            'goto': {
+                'text': {
+                    '@': 'Goto Symbol',
+                    ':': 'Goto Line',
+                    '#': 'Seach Keywords'
+                },
+                'show_files': {
+                    True: 'Goto Anything'
+                }
+            },
+            'command_palette': 'Command Palette'
+        }
+    },
+    'show_panel': {
+        'panel': {
+            'console': 'Show Console',
+            'find_in_files': 'Find in Files',
+            'output.exec': 'Show Build Results',
+            'find': 'Find...',
+            'replace': 'Find and Replace',
+            'incremental_find': {
+                'reverse': {
+                    True: 'Incremental Find (reverse)',
+                    False: 'incremental Find'
+                }
+            }
+        }
+    },
+    'run_zen_action': {
+        'action': 'Zen Action: {pvalue}'
+    },
+    'insert_snippet': {
+        'contents': 'Insert Snippet: {value}',
+        'name': 'Insert Snippet: {value}'
+    },
+    'run_macro_file': {
+        'file': 'Run Macro File: {filename}'
+    },
+    'expand_selection': {
+        'to': 'Expand Selection to {value}'
+    },
+    'open_file': {
+        'file': {
+            '${packages}/User/Preferences.sublime-settings': 'User Preferences'
+        }
+    }
+}
+
+
 class CheatSheetCommand(sublime_plugin.TextCommand):
     """
-    keymap manager for plugins
+    Cheat sheet for keyboard shortcuts
     """
-    osname = sublime.platform()
-    ctrlname = "ctrl"
-    #ctrlname is cmd on macos
-    if osname.lower() == "osx":
-        ctrlname = "cmd"
-    #add some default very usefull commands
-    defaultCommand = [
-        {"name": "Goto Anything...", "keys": [ctrlname + "+p"], "command": "show_overlay", "args": {"overlay": "goto", "show_files": True}},
-        {"name": "Command Palette", "keys": [ctrlname + "+shift+p"], "command": "show_overlay", "args": {"overlay": "command_palette"}},
-        {"name": "Goto Symbol...", "keys": [ctrlname + "+r"], "command": "show_overlay", "args": {"overlay": "goto", "text": "@"}},
-        {"name": "Goto Line...",  "keys": [ctrlname + "+g"], "command": "show_overlay", "args": {"overlay": "goto", "text": ":"}},
-        {"name": "Search Keywords", "keys": [ctrlname + "+;"], "command": "show_overlay", "args": {"overlay": "goto", "text": "#"}},
-        {"name": "Show Console",  "keys": [ctrlname + "+`"],  "command": "show_panel", "args": {"panel": "console", "toggle": True}}
-    ]
+    #add some default very usefull commands;
+    defaultCommand = []
     #installed plugins list
     plugins = None
 
@@ -79,7 +180,7 @@ class CheatSheetCommand(sublime_plugin.TextCommand):
         plugins = []
         global_ignored = sublime.load_settings('Global.sublime-settings').get('ignored_packages')
         ignored_packages = set(settings.get("ignored_packages") + global_ignored)
-        single_max_nums = int(settings.get("single_max_nums") or 3)
+        single_max_nums = int(settings.get("single_max_nums") or -1)
         for name in dirs:
             if name in ignored_packages:
                 continue
@@ -87,8 +188,6 @@ class CheatSheetCommand(sublime_plugin.TextCommand):
             if not os.path.isdir(dir):
                 continue
             platform = sublime.platform().lower().title()
-            if platform == 'Osx':
-                platform = 'OSX'
             keymapFile = dir + "Default (" + platform + ").sublime-keymap"
             if not os.path.isfile(keymapFile):
                 keymapFile = dir + "Default.sublime-keymap"
@@ -97,15 +196,15 @@ class CheatSheetCommand(sublime_plugin.TextCommand):
             #plugins.append(keymapFile)
             with open(keymapFile) as f:
                 content = f.read()
-                # remove comments
+                # remove comments since python JSON does not handle them
                 blockcomment = re.compile('/\*.*?\*/', re.DOTALL | re.MULTILINE)
                 content = re.sub(blockcomment, '', content)
                 content = re.sub('//.*?\n', '', content)
-            try:
-                jsonData = json.loads(content)
-            except ValueError as e:
-                print str(e)
-                continue
+                try:
+                    jsonData = json.loads(content)
+                except ValueError as e:
+                    print str(e)
+                    continue
 
             if not isinstance(jsonData, list):
                 continue
@@ -127,7 +226,7 @@ class CheatSheetCommand(sublime_plugin.TextCommand):
                         keys = prettifyKeys(keys)
                     command = item["command"]
                     item["name"] = name
-                    cmd = prettifySnake(command)
+                    cmd = item.get('description') or getCommandDisplayName(command, item.get('args'))
                     title = u'{0} - {1}'.format(name, keys)
                     plugins.append([cmd, title])
                     self.plugins.append(item)
